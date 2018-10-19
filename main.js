eeed91ff1c0b8f27bc6a6e93a2488679997e068c
kendrickjdaniel <kendrickjdaniel@gmail.com>
2018-10-18 20:48:33 -0400

    added a JS file

---
+++ main.js
@@ -0,0 +1,8 @@
+let age;
+console.log("age is currently:" + age);
+// declare a variable named "age"
+age = 29;
+console.log("age is currently:" + age);
+age = 29 + 1;
+console.log("age is currently:" + age);
+
