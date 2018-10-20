9569fa84e2548be384cbbdb53e260487a95c0bf7
kendrickjdaniel <kendrickjdaniel@gmail.com>
2018-10-20 13:41:27 -0400

    added two JS functions

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-let age;
-console.log("age is currently:" + age);
-// declare a variable named "age"
-age = 29;
-console.log("age is currently:" + age);
-age = 29 + 1;
-console.log("age is currently:" + age);
+let userName = prompt("What is your name?");
+let nameConfirm = confirm("Just Checking, is " + userName + " your real name?");
+// confirm leads to null or ok 
+if (nameConfirm){
+    alert("Welcome to my page," + userName + "!");
+}
+else{
+    userName = prompt("Okay, what is your name?")
+}
 
+console.log("The user's name is " + userName + ".");
